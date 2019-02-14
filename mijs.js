ef0dcb25766c5ebc908800ef9fa4b2e14f26333b
Monica Gomez Avila <mgavila@beta14.aulas.gsyc.urjc.es>
2019-02-14 10:51:39 +0100

    Coge números y los pone en el display. Intento de suma

--- mijs.js
+++ mijs.js
@@ -1,10 +1,51 @@
 function main(){
   console.log("que comiencen los juegos del js");
 
-  var boton = document.getElementById('boton')
-  boton.onclick = () => {
-    console.log("click");
+  var boton1 = document.getElementById('boton1');
+  var boton2 = document.getElementById('boton2');
+  var boton3 = document.getElementById('boton3');
+  var suma = document.getElementById('suma');
+
+
+  var operaciondisplay = "";
+  var numero = "";
+  var array_oper = [];
+
+  boton1.onclick = () => {
     var display = document.getElementById('display');
-    display.innerHTML = "NUEVO TEXTO";
+    operaciondisplay = operaciondisplay + "1";
+    numero = numero +"1";
+    display.innerHTML = operaciondisplay;
   }
+
+  boton2.onclick = () => {
+    var display = document.getElementById('display');
+    operaciondisplay = operaciondisplay + "2";
+    numero = numero +"2";
+    display.innerHTML = operaciondisplay;
+  }
+
+  boton3.onclick = () => {
+    var display = document.getElementById('display');
+    operaciondisplay = operaciondisplay + "3";
+    numero = numero +"3";
+    display.innerHTML = operaciondisplay;
+  }
+
+  suma.onclick = () => {
+    array_oper.push(numero);
+    numero = "";
+    array_oper.push("+");
+    var display = document.getElementById('display');
+    operaciondisplay = operaciondisplay + "+";
+    display.innerHTML = operaciondisplay;
+  }
+
+  resultado.onclick = () => {
+    var display = document.getElementById('display');
+    operaciondisplay = operaciondisplay + "=";
+    display.innerHTML = operaciondisplay;
+    console.log(operaciondisplay);
+  }
+
 }
